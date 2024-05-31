f75cf5b0fdddae15fdb73d087c4f2ed99801a3c8
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-31 02:16:11 -0300

    criando rota de upload de imagem

--- routes/routes.js
+++ routes/routes.js
@@ -1,12 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/users");
+const multer = require("multer");
+
+//image upload
+let storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, "./uploads");
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    },
+});
+
+let upload = multer({
+    storage: storage,
+}).single("imagem");
 
 router.get("/", (req, res) => {
-    res.render("index", { title: "Home Page" })
+    res.render("index", { title: "Home Page" });
 });
 
 router.get("/add", (req, res) => {
-    res.render("add_users", { title: "Adicionar Usuários" })
+    res.render("add_users", { title: "Adicionar Usuários" });
 });
 
 module.exports = router;
