48d532fef4aa7009fc46c0ce114112a19481d214
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-30 21:28:57 -0300

    conexao com o mongodb

--- main.js
+++ main.js
@@ -7,6 +7,13 @@ const session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+//database connection
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log("Conectado ao banco de dados!"));
+
+
 app.get("/", (req, res) => {
     res.send("Olá, mundo!");
 });
