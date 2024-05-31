f8a4d18a1070dfdf3b00e57103b8229ff11b6f37
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-30 22:01:28 -0300

    Modelo de criacao de usuario

---
+++ models/users.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const userSchema = new mongoose.Schema({
+    nome: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    telefone: {
+        type: String,
+        required: true,
+    },
+    imagem: {
+        type: String,
+        required: true,
+    },
+    created: {
+        type: Date,
+        required: true,
+        default: Date.now, 
+    }
+});
+module.exports = mongoose.model("User", userSchema);
