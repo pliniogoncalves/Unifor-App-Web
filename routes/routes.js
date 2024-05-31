71cf7195a3cd8d9bf6d12be3037e8b2bd96cc9b8
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-31 03:14:15 -0300

    criando a rota de adicionar usuario no banco e mensagem de confirmação

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require("../models/users");
 const multer = require("multer");
 
-//image upload
+// Configuração do multer para upload de imagem
 let storage = multer.diskStorage({
     destination: function(req, file, cb) {
         cb(null, "./uploads");
@@ -16,6 +16,27 @@ let storage = multer.diskStorage({
 let upload = multer({
     storage: storage,
 }).single("imagem");
+
+// Rota para adicionar um usuário ao banco de dados
+router.post("/add", upload, async (req, res) => {
+    const user = new User({
+        nome: req.body.nome,
+        email: req.body.email,
+        telefone: req.body.telefone,
+        imagem: req.file.filename,
+    });
+    
+    try{
+        await user.save();
+            req.session.message = {
+                    type: "sucess", 
+                    message: "Usuário adicionado com sucesso!",
+                };
+                res.redirect("/");
+    }catch (err) {
+        res.json({message: err.message, type: "danger"});
+    }  
+});
 
 router.get("/", (req, res) => {
     res.render("index", { title: "Home Page" });
