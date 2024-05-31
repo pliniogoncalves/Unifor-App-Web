5939c4746e46170cdd63c87c93f798ef3e34e0ee
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-31 01:42:05 -0300

    pagina add usuario

--- routes/routes.js
+++ routes/routes.js
@@ -5,4 +5,8 @@ router.get("/", (req, res) => {
     res.render("index", { title: "Home Page" })
 });
 
+router.get("/add", (req, res) => {
+    res.render("add_users", { title: "Adicionar Usuários" })
+});
+
 module.exports = router;
