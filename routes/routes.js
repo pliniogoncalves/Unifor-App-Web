c468a810e18be092746093e77ad06a80cb815c51
Plinio Gonçalves <plinioricardogoncalves@gmail.com>
2024-05-31 19:00:35 -0300

    Finalizando o editar e atualizar usuário

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/users");
 const multer = require("multer");
+const fs = require("fs");
 
 // Configuração do multer para upload de imagem
 let storage = multer.diskStorage({
@@ -53,6 +54,58 @@ router.get("/", async (req, res) => {
 
 router.get("/add", (req, res) => {
     res.render("add_users", { title: "Adicionar Usuários" });
+});
+
+// Rota para editar um usuario no banco de dados
+router.get("/edit/:id", async (req, res) => {
+    let id = req.params.id;
+    try {
+        const user = await User.findById(id);
+        if (user == null) {
+            res.redirect("/");
+        } else {
+            res.render("edit_users", {
+                title: "Editar Usuário",
+                user: user,
+            });
+        }
+    } catch (err) {
+        res.redirect("/");
+    }
+});
+
+// Rota para atualizar um usuario no banco de dados
+// Rota para atualizar um usuário no banco de dados
+router.post("/update/:id", upload, async (req, res) => {
+    let id = req.params.id;
+    let new_image = "";
+
+    if (req.file) {
+        new_image = req.file.filename;
+        try {
+            fs.unlinkSync("./uploads/" + req.body.old_image);
+        } catch (err) {
+            console.log(err);
+        }
+    } else {
+        new_image = req.body.old_image;
+    }
+
+    try {
+        await User.findByIdAndUpdate(id, {
+            nome: req.body.nome,
+            email: req.body.email,
+            telefone: req.body.telefone,
+            imagem: new_image,
+        });
+        req.session.message = {
+            type: "success",
+            message: "Usuário atualizado com sucesso!",
+        };
+        res.redirect("/");
+    } catch (err) {
+        res.json({ message: err.message, type: "danger" });
+    }
 });
 
 module.exports = router;
